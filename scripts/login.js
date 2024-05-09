7e552d74b239a1aec1b69c20f5a0935f69f8299a
mohammadtehreem <tehreemmohammad@gmail.com>
2024-05-10 02:53:41 +0530

    sign up validation done

--- scripts/login.js
+++ scripts/login.js
@@ -17,6 +17,7 @@ loginLink.addEventListener("click", (e) => {
   loginForm.style.display = "block";
 });
 
+let userURL = "http://localhost:3000/users";
 let loginUsername = document.querySelector(".login-username");
 let loginPassword = document.querySelector(".login-password");
 let signupName = document.querySelector(".signup-name");
@@ -29,41 +30,62 @@ let loginBtn = document.querySelector(".login-btn");
 
 signupBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
-  if (
-    signupName.value == "" ||
-    signupUsername.value == "" ||
-    signupPassword.value == "" ||
-    signupEmail.value == "" ||
-    signupNumber.value == ""
-  ) {
-    alert("Fill all the details!");
-  } else {
-    localStorage.setItem("loginCheck", JSON.stringify("true"));
-    let obj = {
-      id: 1,
-      name: signupName.value,
-      username: signupUsername.value,
-      password: signupPassword.value,
-      email: signupEmail.value,
-      mobile: signupNumber.value,
-    };
-    saveUserData(obj);
-  }
+  saveUserData();
+  // localStorage.setItem("loginCheck", JSON.stringify("true"));
 });
 
-async function saveUserData(obj) {
-  try {
-    let res = await fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(obj),
-    });
-    let data = await res.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
+async function saveUserData() {
+  if (
+    signupName.value &&
+    signupUsername.value &&
+    signupPassword.value &&
+    signupEmail.value &&
+    signupNumber.value
+  ) {
+    if (signupPassword.value.length < 5) {
+      alert("Password must be 5 characters or more!");
+    } else if (await verifyUserData()) {
+      alert("User is already registered. Please Log in");
+      signupForm.style.display = "none";
+      loginForm.style.display = "block";
+    } else {
+      let newObj = {
+        name: signupName.value,
+        username: signupUsername.value,
+        password: signupPassword.value,
+        email: signupEmail.value,
+        mobile: signupNumber.value,
+      };
+      try {
+        await fetch(userURL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newObj),
+        });
+        alert("User Registered Successfuly!");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  } else {
+    alert("Fill in all the fields!");
   }
+}
+
+async function verifyUserData() {
+  try {
+    let res = await fetch(userURL);
+    let data = await res.json();
+    for (let element of data) {
+      if (element.username == signupUsername.value) {
+        return true;
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  return false;
 }
