62be75275968e9bb59934c8a18df3ee172ca77b0
aniyant <kumarsunny30066@gmail.com>
2024-05-10 00:10:13 +0530

    updated

--- scripts/category.js
+++ scripts/category.js
@@ -1,95 +1,133 @@
-let str = `
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
-Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their 
-exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+// let filters = document.getElementById("filters-wrapper");
 
-Where can I get some?
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in 
-some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to 
-use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+// let filters_category = ["title","category","description","rating","price","oldPrice","image","material","brand","gender","sales","fit"]
+// filters.addEventListener("onload",(e)=>{
 
-All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first 
-true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model 
-sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always 
-free from repetition, injected humour, or non-characteristic words etc.
+// })
+let str= `{
+    "products": [
+        {
+            "title": "Men's Casual Cargo Pants",
+            "category": "cargo",
+            "description": "Stay cool and comfortable with our Women's Casual Linen Pants.",
+            "rating": "4.0",
+            "price": "699",
+            "oldPrice": "899",
+            "image": "https://images.bewakoof.com/t640/men-s-brown-boxer-628781-1707976186-1.jpg",
+            "material": "premium blended fabric",
+            "brand": "Bushirt",
+            "gender": "men",
+            "sales": "2389",
+            "fit": "Regular Fit"
+        },
+        {
+            "title": "Women's Floral Dress",
+            "category": "dress",
+            "description": "Stay cool and comfortable with our Women's Casual Linen Pants.",
+            "rating": "4.8",
+            "price": "1299",
+            "oldPrice": "1499",
+            "image": "https://images.bewakoof.com/t640/men-s-chocolate-brown-printed-oversized-hooded-vest-627843-1710335791-1.jpg",
+            "material": "100% cotton",
+            "brand": "Rigo",
+            "gender": "women",
+            "sales": "3856",
+            "fit": "Slim Fit"
+        },
+        {
+            "title": "Women's Cropped Denim Jacket",
+            "category": "jackets",
+            "description": "Stay cool and comfortable with our Women's Casual Linen Pants.",
+            "price": "1199",
+            "oldPrice": "1399",
+            "image": "https://images.bewakoof.com/t640/men-printed-co-ord-set-7-619985-1709805508-1.jpg",
+            "material": "denim fabric",
+            "brand": "Bewakoof®",
+            "gender": "women",
+            "sales": "14200",
+            "fit": "Regular Fit"
+        },
+        {
+            "title": "Men's Classic Crewneck T-Shirt",
+            "category": "t-shirt",
+            "description": "Keep it simple and stylish with our Men's Classic Crewneck T-Shirt. This t-shirt features a classic crewneck design and soft cotton fabric, perfect for everyday wear.",
+            "price": "599",
+            "oldPrice": "699",
+            "image": "https://images.bewakoof.com/t640/men-s-brown-striped-oversized-polo-t-shirt-617096-1707200102-1.jpg",
+            "material": "soft cotton fabric",
+            "brand": "Rigo",
+            "gender": "men",
+            "sales": "15678",
+            "fit": "Regular Fit"
+        },
+        {
+            "title": "Women's Casual Linen Pants",
+            "category": "pants",
+            "description": "Stay cool and comfortable with our Women's Casual Linen Pants.",
+            "price": "899",
+            "oldPrice": "999",
+            "image": "https://images.bewakoof.com/t640/men-s-white-striped-oversized-polo-t-shirt-617090-1707200069-1.jpg",
+            "material": "linen fabric",
+            "brand": "Campus Sutra",
+            "gender": "women",
+            "sales": "16900",
+            "fit": "Regular Fit"
+        }
+    ]
+}`;
+let products = JSON.parse(str);
+console.log(products);
+renderCards(products.products);
 
-5
-    paragraphs
-    words
-    bytes
-    lists
-    Start with 'Lorem
-ipsum dolor sit amet...'
+function createCard(obj,ind){
+    console.log(obj);
+    let div = document.createElement("div");
+    div.className = "card";
 
-Translations: Can you help translate this site into a foreign language ? Please email us with details if you can help.
-There is a set of mock banners available here in three colours and in a range of standard banner sizes:
-BannersBannersBanners
-Donate: If you use this site regularly and would like to help keep the site on the Internet, 
-please consider donating a small sum to help pay for the hosting and bandwidth bill. 
-There is no minimum donation, any sum is appreciated - click here to donate using PayPal. 
-Thank you for your support.
-Donate Bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDjb4tyF
-NodeJS Python Interface GTK Lipsum Rails .NET Groovy
+    let img_div = document.createElement("div");
+    div.setAttribute("id","image-div");
 
-The standard Lorem Ipsum passage, used since the 1500s
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    let image = document.createElement("img");
+    image.setAttribute("src",obj.image);
+    image.style.display = "block";
+    image.className="product-image";
 
-Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    let fit = document.createElement("span");
+    fit.className="fit";
+    fit.innerText = obj.fit;
 
-1914 translation by H. Rackham
-"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain 
-was born and I will give you a complete account of the system, and expound the actual teachings of 
-the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or 
-avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue 
-pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who 
-loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally 
-circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial 
-example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from 
-it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying 
-consequences, or one who avoids a pain that produces no resultant pleasure?"
+    let rating = document.createElement("span");
+    rating.className = "rating";
+    rating.innerHTML = `<i class="fa-solid fa-star fa-xs" style="color: #FFD43B;"></i>${obj.rating}`;
 
-Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum 
-deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, 
-similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. 
-Et harum quidem rerum facilis est et expedita distinctio. 
-Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime 
-placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. 
-Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates 
-repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut 
-reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+    //appending image,fit and rating into img_div;
+    img_div.append(image,fit,rating);
 
-1914 translation by H. Rackham
-"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
-demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain 
-and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness 
-of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and 
-easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our 
-being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain 
-circumstances and owing to the claims of duty or the obligations of business it will frequently occur that 
-pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters 
-to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures 
-pains to avoid worse pains."`;
+    let brand = document.createElement("div");
+    brand.className = "title";
+    brand.innerHTML = `<span class="title">${obj.title}</span><span class="wishlist"><i class="fa-regular fa-heart fa-lg" style="color: #a39f9f;"></i></span>`;
+    
 
-let element = document.getElementById("content-wrapper");
-let a = str.split(".");
-let index = 0;
-window.addEventListener("scroll",(e)=>{
-    let clientHeight = document.documentElement.clientHeight;
-    let scrollHeight = document.documentElement.scrollHeight;
-    let scrollTop = document.documentElement.scrollTop;
-    if(index < a.length){
-        console.log(index);
-        // if(scrollTop >= scrollTop+200){
-            let div = document.createElement("div");
-            div.innerText = a[index++];
-            element.appendChild(div);
-        // }
-    }
-    else{
-        window.removeEventListener();
-    }
-})
+    let category = document.createElement("p");
+    category.className = "category";
+    category.innerText = `${obj.category}`;
+
+    let price = document.createElement("p");
+    price.className = "price";
+    price.innerHTML = `${obj.price}<s class="old-price">999</s>`
+
+    let material = document.createElement("p");
+    material.className = "material";
+    material.innerText = `${obj.material}`;
+
+    div.append(img_div,brand,category,price,material);
+    return div;
+}
+
+function renderCards(data){
+    let dataListWrapper = document.getElementById("data-list-title-wrapper");
+    data.forEach((element,index) => {
+        let card  = createCard(element,index);
+        dataListWrapper.append(card);
+    });
+}
