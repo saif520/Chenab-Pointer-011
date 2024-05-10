0eb92f21be4adf93dc704475b0c736f242c36b80
mohammadtehreem <tehreemmohammad@gmail.com>
2024-05-10 22:29:52 +0530

    new branch

--- scripts/login.js
+++ scripts/login.js
@@ -98,7 +98,7 @@ loginBtn.addEventListener("click", (e) => {
 async function createLoginSession() {
   if (await userLogin()) {
     localStorage.setItem("loginToken", JSON.stringify("true"));
-    console.log("done");
+    alert("done");
   } else {
     alert("Wrong Credentials!");
   }
