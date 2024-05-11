3397e51b4b05e5f29be98d4b2085dd015c7afa32
saif520 <saifuddin2k17@gmail.com>
2024-05-11 13:36:41 +0530

    footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
-let container = document.getElementById("container")
+// let container = document.getElementById("container")
 
-container.innerText="Home thing";
+// container.innerText="Home thing";
+
+
+let fSubscribe=document.getElementById("f-subscribe-btn");
+fSubscribe.addEventListener("click",()=>{
+    fSubscribe.innerText=`subscribed`;
+    console.log("subscribed");
+})
