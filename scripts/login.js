2302c7d5fa200c2761f5320c772878a0fcb80504
Chaitanya-05 <cdakhale@gmail.com>
2024-05-13 01:39:31 +0530

    cart update and navbar add

--- scripts/login.js
+++ scripts/login.js
@@ -15,7 +15,7 @@ loginLink.addEventListener("click", (e) => {
   loginForm.style.display = "block";
 });
 
-let userURL = "http://localhost:3000/users";
+let userURL = "https://server-ve5q.onrender.com/users";
 let loginUsername = document.querySelector(".login-username");
 let loginPassword = document.querySelector(".login-password");
 let signupName = document.querySelector(".signup-name");
